eaa9a8ea21018ec54d2a57b1ee502dda7d895140
Oleksii Sokoliuk <oleksii.sokoliuk@mao.kiev.ua>
2025-04-27 14:11:56 +0100

    handle IO monad

--- src/HMF.hs
+++ src/HMF.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module HMF where
 
 import Cosmology
 import Data.Maybe
+import qualified Data.Text as T
 import qualified Data.Vector as V
 import Numeric.Tools.Differentiation
 import Numeric.Tools.Integration
 import Numeric.Tools.Interpolation
+import qualified Safe
 
 -- Define an HMF datatype, consisting of two choices
 data HMF_kind
@@ -41,12 +45,13 @@ rh cosmology mh =
 
 -- | Produces a matter power spectrum at the linear level
 -- To be imported from CAMB
-powerSpectrum :: Wavenumber -> Redshift -> Double
-powerSpectrum k z =
-  let xPoints = [1, 2, 3, 4]
-      yPoints = [2, 4, 6, 8]
-      interp = linearInterp $ tabulate (uniformMesh (1e-4, 1e4) 4) (V.fromList xPoints)
-   in interp `at` k
+powerSpectrum :: FilePath -> IO [Double]
+powerSpectrum filepath =
+  do
+    content <- readFile filepath
+    let pk_arr :: [Double]
+        pk_arr = fmap read (lines content)
+    return pk_arr
 
 -- | Window function, used to derive the cosmic variance
 -- You have a choice of two different ones, namely:
@@ -63,62 +68,80 @@ windowFunction cosmology w_kind k mh =
 
 -- | Cosmic variance squared, usually referred to as sigma^2(R,z)
 -- Derived by integrating a matter power spectrum and a window function
-cosmicVarianceSq :: ReferenceCosmology -> Mhalo -> Redshift -> W_kind -> Double
-cosmicVarianceSq cosmology mh z w_kind =
-  let integrand :: Wavenumber -> Double
-      integrand k =
-        (k ** 2 / (2 * pi ** 2))
-          * (powerSpectrum k z)
-          * (windowFunction cosmology w_kind k mh) ** 2
+cosmicVarianceSq :: FilePath -> ReferenceCosmology -> Mhalo -> Redshift -> W_kind -> IO Double
+cosmicVarianceSq filepath cosmology mh z w_kind =
+  do
+    pk_arr <- powerSpectrum filepath
+    let interp :: LinearInterp UniformMesh
+        interp = linearInterp $ tabulate (uniformMesh (1e-4, 1e4) (length pk_arr)) (V.fromList pk_arr)
 
-      params :: QuadParam
-      params = QuadParam {quadPrecision = 1e-9, quadMaxIter = 20}
+        integrand :: Wavenumber -> Double
+        integrand k =
+          (k ** 2 / (2 * pi ** 2))
+            * (interp `at` k)
+            * (windowFunction cosmology w_kind k mh) ** 2
 
-      result :: Maybe Double
-      result = quadRes $ quadSimpson params (1e-4, 1e4) integrand
-   in fromMaybe 0.0 result
+        params :: QuadParam
+        params = QuadParam {quadPrecision = 1e-9, quadMaxIter = 1000}
+
+        result :: Maybe Double
+        result = quadRes $ quadSimpson params (1e-4, 1e4) integrand
+    return $ fromMaybe 0.0 result
 
 -- | First-crossing distribution, crucial for the derivation of a
 -- Halo Mass Function afterwards, again you have a choice of two:
 --    * Sheth-Tormen
 --    * Tinker
 -- We are planning to add more options in the near future
-firstCrossing :: ReferenceCosmology -> HMF_kind -> W_kind -> Mhalo -> Redshift -> Double
-firstCrossing cosmology h_kind w_kind mh z =
-  let sigma :: Double
-      sigma = sqrt $ cosmicVarianceSq cosmology mh z w_kind
+firstCrossing :: FilePath -> ReferenceCosmology -> HMF_kind -> W_kind -> Mhalo -> Redshift -> IO Double
+firstCrossing filepath cosmology h_kind w_kind mh z =
+  do
+    sigma <- cosmicVarianceSq filepath cosmology mh z w_kind
 
-      nu :: Double
-      nu = 1.686 / sigma
+    let sigma_eval :: Double
+        sigma_eval = sqrt sigma
 
-      a_T, a_ST, p, a, b, c :: Double
-      (a_T, a_ST, p, a, b, c) = (0.186, 0.3222, 0.3, 1.47, 2.57, 1.19)
-   in case h_kind of
-        Tinker -> a_T * ((sigma / b) ** (-a) + 1) * exp (-c / sigma ** 2)
-        ST -> a_ST * sqrt (2 * nu ** 2 / pi) * (1 + nu ** (-2 * p)) * exp (-nu ** 2 / 2)
+        nu :: Double
+        nu = 1.686 / sigma_eval
+
+        a_T, a_ST, p, a, b, c :: Double
+        (a_T, a_ST, p, a, b, c) = (0.186, 0.3222, 0.3, 1.47, 2.57, 1.19)
+    return $ case h_kind of
+      Tinker -> a_T * ((sigma_eval / b) ** (-a) + 1) * exp (-c / sigma_eval ** 2)
+      ST -> a_ST * sqrt (2 * nu ** 2 / pi) * (1 + nu ** (-2 * p)) * exp (-nu ** 2 / 2)
 
 -- | The Halo Mass Function (HMF) itself, uses most of the functions
 -- defined within this module and a differentiation library
-haloMassFunction :: ReferenceCosmology -> HMF_kind -> W_kind -> [Mhalo] -> Redshift -> [Double]
-haloMassFunction cosmology h_kind w_kind mh_arr z =
-  let (h0, om0, omb0, c, gn) = unpackCosmology cosmology
+haloMassFunction :: FilePath -> ReferenceCosmology -> HMF_kind -> W_kind -> [Mhalo] -> Redshift -> IO [Double]
+haloMassFunction filepath cosmology h_kind w_kind mh_arr z =
+  do
+    sigma_arr <- mapM (\mh -> cosmicVarianceSq filepath cosmology mh z w_kind) mh_arr
+    first_crossing_arr <- mapM (\mh -> firstCrossing filepath cosmology h_kind w_kind mh z) mh_arr
 
-      diff_func :: Double -> Double
-      diff_func mh = log $ sqrt (cosmicVarianceSq cosmology mh z w_kind)
-      dsdm = (\mh -> diffRes $ diffRichardson diff_func 1.0 mh) <$> mh_arr
+    let (h0, om0, omb0, c, gn) = unpackCosmology cosmology
 
-      dsdm :: [Double]
-      dsdlogm = zipWith (*) mh_arr dsdm
+        interp_sigma :: LinearInterp UniformMesh
+        interp_sigma =
+          linearInterp $
+            tabulate
+              (uniformMesh (minimum mh_arr, maximum mh_arr) (length mh_arr))
+              (V.fromList sigma_arr)
 
-      rho_mean :: Double
-      rho_mean = 3 * h0 ** 2 * om0 / (8 * pi * gn)
+        diff_func :: Double -> Double
+        diff_func mh = log . sqrt $ interp_sigma `at` mh
+        dsdm = (\mh -> diffRes $ diffRichardson diff_func 1.0 mh) <$> mh_arr
 
-      first_crossing :: [Double]
-      first_crossing = (\mh -> firstCrossing cosmology h_kind w_kind mh z) <$> mh_arr
+        dsdm :: [Double]
+        dsdlogm = zipWith (*) mh_arr dsdm
 
-      fdsdlogm :: [Double]
-      fdsdlogm = zipWith (*) dsdlogm first_crossing
-   in zipWith (*) fdsdlogm ((* (-rho_mean)) <$> mh_arr)
+        rho_mean :: Double
+        rho_mean = 3 * h0 ** 2 * om0 / (8 * pi * gn)
+
+        fdsdlogm :: [Double]
+        fdsdlogm = zipWith (*) dsdlogm first_crossing_arr
+    return $ zipWith (*) fdsdlogm ((* (-rho_mean)) <$> mh_arr)
 
 main :: IO ()
-main = print $ haloMassFunction (MkCosmology {h0' = 69, om0' = 0.305, ob0' = 0.05, c' = 3e5, gn' = 1e-11}) ST TopHat [1e10, 1e11] 0
+main = do
+  x <- haloMassFunction "camb.txt" (MkCosmology {h0' = 69, om0' = 0.305, ob0' = 0.05, c' = 3e5, gn' = 1e-11}) ST TopHat [1e10, 1e11] 0
+  print x
